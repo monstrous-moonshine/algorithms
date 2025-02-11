cf88a9c2bd6843470c72e6e4b48047c25009e7db
Khalid Bin Walid <kbwalid26@gmail.com>
2025-02-11 23:04:33 +0900

    add digraph type

--- graph/graph.h
+++ graph/graph.h
@@ -1,6 +1,36 @@
 #include <istream>
 #include <vector>
 
+struct Digraph {
+    int V, E{};
+    std::vector<std::vector<int>> adj;
+    Digraph(int v) : V(v), adj(v) {}
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+        E++;
+    }
+    static Digraph from(std::istream &is) {
+        int V, E;
+        is >> V >> E;
+        Digraph out(V);
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            is >> u >> v;
+            out.addEdge(u, v);
+        }
+        return out;
+    }
+    Digraph reverse() const {
+        Digraph out(V);
+        for (int u = 0; u < V; u++) {
+            for (auto &&v : adj[u]) {
+                out.addEdge(v, u);
+            }
+        }
+        return out;
+    }
+};
+
 struct Edge {
     int u, v;
     float weight;
